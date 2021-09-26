a45a2f9523d3cf974ef5c117fd3effa19016cc3d
eetann <hideharu092@gmail.com>
2021-09-26 12:52:47 +0900

    ショートカットキーの実装

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,3 +1,34 @@
+async function getTodayPage() {
+  let date = new Date();
+  console.log(date);
+  // let dateStr: string = date.toISOString().slice(0, 10)
+  // const response = await notion.databases.query({
+  //   database_id: "DAILY_DATABASE_ID",
+  //   filter: {
+  //     property: 'Date',
+  //     date: {
+  //       equals: dateStr,
+  //     },
+  //   },
+  // });
+  // const gasUrl = "https://script.google.com/macros/s/xxx/exec";
+
+  // fetch(gasUrl)
+  // .then(response => {
+  //   return response.text();
+  // })
+  // .then(json => {
+  //   console.log(`GASからのレスポンス: ${json}`);
+  //   // callback(JSON.parse(json));
+  // });
+}
+
+chrome.commands.onCommand.addListener((command: string) => {
+  if (command == "today") {
+    getTodayPage();
+  }
+});
+
 chrome.runtime.onMessage.addListener(
   (request, _, callback) => {
     console.log(`バックグラウンドで受け取ったもの: ${request.message}`);
