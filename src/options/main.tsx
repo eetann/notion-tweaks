8c8bd2efddafaef132caed618c7697ad5404ebdf
eetann <hideharu092@gmail.com>
2021-10-08 22:03:24 +0900

    OptionsページでReact Hook Formを採用

--- src/options/main.tsx
+++ src/options/main.tsx
@@ -2,7 +2,8 @@
 // "NTION_Z10N_ID": "",
 import * as React from "react";
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+import {useForm, Controller} from "react-hook-form";
+import {TextField, Button, Grid} from '@mui/material';
 
 // const getStorage = (key=null) => new Promise(resolve => {
 //   chrome.storage.local.get(key, (data) => {resolve(data)});
@@ -14,7 +15,42 @@ import Button from '@mui/material/Button';
 // 
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  const {control, handleSubmit} = useForm();
+  const onSubmit = () => console.log("obSubmit!");
+  return (
+    <Grid container>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="firstName"
+          control={control}
+          defaultValue=""
+          render={({field}) => <Grid item>
+            <TextField id="gas_url" label="GAS URL" variant="outlined" margin="normal" {...field} />
+          </Grid>
+          }
+        />
+        <Controller
+          name="firstName"
+          control={control}
+          defaultValue=""
+          render={({field}) =>  <Grid item>
+            <TextField id="notion_daily_id" label="Daily database ID" variant="outlined" margin="normal" {...field} />
+          </Grid>
+          }
+        />
+        <Controller
+          name="firstName"
+          control={control}
+          defaultValue=""
+          render={({field}) =>  <Grid item>
+            <TextField id="notion_z10n_id" label="z10n database ID" variant="outlined" margin="normal" {...field} />
+            </Grid>
+          }
+        />
+        <Button type="submit" variant="contained">Resister</Button>
+      </form>
+    </Grid>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#app'));
