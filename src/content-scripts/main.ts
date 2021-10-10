d270191a6a5123c7592ea7730303262551e5087b
eetann <hideharu092@gmail.com>
2021-10-10 22:26:53 +0900

    タイムスタンプをctrl+qでクリップボードにコピー

--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -1,9 +1,16 @@
 import './index.css';
 
-console.log("content.js開始");
-
-chrome.runtime.sendMessage({
-  message: "contentからbackgrouodに送るもの(無くてもいい)"
-}, response => {
-  console.log(`backgroundからの戻り値: ${JSON.stringify(response)}`);
+document.body.addEventListener("keyup", event => {
+  if (event.key === "q" && event.ctrlKey) {
+    const now = new Date();
+    const nowStr = now.getHours().toString() + ":" + now.getMinutes().toString() + " ";
+    navigator.clipboard.writeText(nowStr);
+    // navigator.clipboard.readText()
+    // .then(function(text){
+    //   pasteArea.textContent = text;
+    // });
+    console.log("ctrl + q");
+  }
 });
+
+console.log("Notion Tweaks!");
