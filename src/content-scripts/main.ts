98b30fab8eab39e4018aa26d6b0f273a6fd331cd
eetann <hideharu092@gmail.com>
2021-10-11 21:26:26 +0900

    クリップボードを使わずにタイムスタンプを挿入

--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -1,15 +1,32 @@
 import './index.css';
 
+function dispatchPaste(target: Element, text: string) {
+  const dataTransfer = new DataTransfer();
+  dataTransfer.setData('text/plain', text);
+  target.dispatchEvent(
+    new ClipboardEvent('paste', {
+      clipboardData: dataTransfer,
+      bubbles: true,
+      cancelable: true
+    })
+  );
+  dataTransfer.clearData();
+}
+
+
+function writeTimeStamp() {
+  const focusElement = document.activeElement;
+  if (focusElement.tagName === "body") {
+    return;
+  }
+  const now = new Date();
+  const nowStr = now.getHours().toString() + ":" + now.getMinutes().toString() + " ";
+  dispatchPaste(focusElement, nowStr);
+}
+
 document.body.addEventListener("keyup", event => {
   if (event.key === "q" && event.ctrlKey) {
-    const now = new Date();
-    const nowStr = now.getHours().toString() + ":" + now.getMinutes().toString() + " ";
-    navigator.clipboard.writeText(nowStr);
-    // navigator.clipboard.readText()
-    // .then(function(text){
-    //   pasteArea.textContent = text;
-    // });
-    console.log("ctrl + q");
+    writeTimeStamp();
   }
 });
 
