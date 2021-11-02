6d19fe41e3583a6e95d03869f6f40e789a2c7c91
eetann <hideharu092@gmail.com>
2021-11-02 21:43:17 +0900

    READMEのリンクを載せた

--- src/options/main.tsx
+++ src/options/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {Box} from '@mui/material';
+import {Box, Link} from '@mui/material';
 import {FormBox} from "./FormBox";
 import {ToggleSettings} from "./ToggleSettings";
 
 const App: React.VFC = () => {
   return (
     <Box sx={{m: 2}}>
+      <Box sx={{my: 4}}>
+        <Link href="https://github.com/eetann/notion-tweaks#readme" variant="h5">
+          README
+        </Link>
+      </Box>
       <FormBox />
       <ToggleSettings />
     </Box>
